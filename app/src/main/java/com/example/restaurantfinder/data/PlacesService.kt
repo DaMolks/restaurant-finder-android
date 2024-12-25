3451ca1fc820da213b33a0d3857c132be47e8f0c
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-25 18:49:46 +0100

    Replace RectangularBounds with LocationBiasRectangularBounds

--- app/src/main/java/com/example/restaurantfinder/data/PlacesService.kt
+++ app/src/main/java/com/example/restaurantfinder/data/PlacesService.kt
@@ -5,12 +5,15 @@ import android.util.Log
 import com.example.restaurantfinder.model.Restaurant
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.model.LocationBias
+import com.google.android.libraries.places.api.model.LocationBiasRectangularBounds
 import com.google.android.libraries.places.api.net.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.model.LocationRestriction
 import kotlinx.coroutines.tasks.await
 
 class PlacesService(private val placesClient: PlacesClient) {
-
+    // ... reste du code identique, mais remplacer RectangularBounds par LocationBiasRectangularBounds
     private val placeFields = listOf(
         Place.Field.NAME,
         Place.Field.ADDRESS,
@@ -27,7 +30,7 @@ class PlacesService(private val placesClient: PlacesClient) {
             Log.d("PlacesService", "Recherche restaurants près de: ${location.latitude}, ${location.longitude}")
             
             val request = FindAutocompletePredictionsRequest.builder()
-                .setLocationBias(RectangularBounds.newInstance(
+                .setLocationBias(LocationBiasRectangularBounds.newInstance(
                     LatLng(location.latitude - 0.01, location.longitude - 0.01),
                     LatLng(location.latitude + 0.01, location.longitude + 0.01)
                 ))
@@ -75,7 +78,7 @@ class PlacesService(private val placesClient: PlacesClient) {
 
             if (location != null) {
                 val request = FindAutocompletePredictionsRequest.builder()
-                    .setLocationBias(RectangularBounds.newInstance(
+                    .setLocationBias(LocationBiasRectangularBounds.newInstance(
                         LatLng(location.latitude - 0.01, location.longitude - 0.01),
                         LatLng(location.latitude + 0.01, location.longitude + 0.01)
                     ))
