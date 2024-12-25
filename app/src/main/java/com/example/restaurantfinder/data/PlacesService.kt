934c151bedf9e3f41d83daff09517247ac8cee4b
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-25 18:43:18 +0100

    Add RectangularBounds import

--- app/src/main/java/com/example/restaurantfinder/data/PlacesService.kt
+++ app/src/main/java/com/example/restaurantfinder/data/PlacesService.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import com.example.restaurantfinder.model.Restaurant
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.model.RectangularBounds
 import com.google.android.libraries.places.api.net.*
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.tasks.await
